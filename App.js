4eec8b245ce69024107b0e60f71e5face108e133
eL Setiyawan <el.setiyawan@gmail.com>
2020-12-24 12:55:39 +0700

    inital

---
+++ App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Grab</Text>
+    </View>
+    // <>
+    //   {/* <StatusBar barStyle="dark-content" /> */}
+    //   <SafeAreaView>
+    //     <ScrollView
+    //       contentInsetAdjustmentBehavior="automatic"
+    //       style={styles.scrollView}>
+    //       <Header />
+    //       {global.HermesInternal == null ? null : (
+    //         <View style={styles.engine}>
+    //           <Text style={styles.footer}>Engine: Hermes</Text>
+    //         </View>
+    //       )}
+    //     </ScrollView>
+    //   </SafeAreaView>
+    // </>
+  );
+};
+
+
+const styles = {
+  
+}
+
+export default App;
